470eb25dcf1454e418e054a35e21ddd7f44c216b
cauaDKZ <166842640+cauaDKZ@users.noreply.github.com>
2024-09-02 13:33:17 -0300

    jghjg

--- scripit.js
+++ scripit.js
@@ -6,44 +6,31 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Pensando em melhorar o desempenho dos atletas olímpicos para as Olimpíadas de 2028 (Los Angeles), o governo brasileiro deveria investir mais no esporte?",
+        enunciado: "Você acha importante a prática de esportes para a saúde física e mental?",
         alternativas: [
             {
-                texto: "Sim, os atletas precisam de incentivos maiores para melhorar seu desempenho",
-                afirmacao: "O incentivo para atletas motiva pessoas que desejam ingressar nesta carreira, principalmente as crianças e adolescentes."
+                texto: "Sim! Praticar esportes é fundamental, contribuindo para uma melhor qualidade de vida",
+                afirmacao: "Além de melhorar a aptidão física, o exercício físico regular também pode melhorar a capacidade cognitiva e reduzir os níveis de ansiedade e estresse em geral."
             },
             {
-                texto: "Não, o governo brasileiro deveria utilizar o dinheiro público em outras áreas",
-                afirmacao: "A situação do Brasil mostra que a carreira de atleta é somente para poucos. Assim, o governo deveria investir o dinheiro público em outras áreas (saúde, educação, saneamento básico.",
+                texto: "Não! Pode ser algo perigoso dependendo a pratica esportiva",
+                afirmacao: "As desvantagens de praticar esportes incluem risco de lesões, sobrecarga física e mental, compromisso de tempo, custos financeiros e pressão para desempenho.",
             }
         ]
     },
     {
-        enunciado: "Como o governo deveria distribuir o dinheiro investido no esporte?",
+        enunciado: "A escola poderia incentivar a prática esportiva para melhorar a saúde física e mental dos alunos?",
         alternativas: [
             {
-                texto: "Aumentando o valor do bolsa-atleta, construção de novas estruturas esportivas em localidades sem nenhuma estrutura, incentivo e apoio financeiro para novos atletas e futuros treinadores",
+                texto: "Sim! Oferecendo uma variedade de atividades, integrando esportes no currículo, promovendo educação sobre saúde, encorajando o trabalho em equipe e oferecendo orientação especializada",
                 afirmacao: "Incentiva os atletas a obterem melhores desempenhos nas competições."
             },
             {
-                texto: "Ampliando os centros de treinamento e nos treinadores.",
-                afirmacao: "É necessário investir na melhoria da infraestrutura existente e na capacitação para treinadores."
+                texto: "Não, pois pode gerar inúmeros gastos que poderiam ser utilizados em outras áreas.",
+                afirmacao: "A desvantagem em incentivar a prática esportiva na escola podem incluir custos adicionais e desmotivar estudantes que não gostam de esportes."
             }
         ]
     },
-    {
-        enunciado: "Como a educação poderia incentivar o esporte para crianças e adolescentes dentro e fora da escola?",
-        alternativas: [
-            {
-                texto: "Aulas de Educação Física permitem a descoberta de novos atletas",
-                afirmacao: "Os profissionais de Educação Física deveriam incentivar a prática esportiva de diversas modalidades, ampliação das aulas de Educação Física, contratação de treinadores para a prática esportiva em período contra-turno."
-            },
-            {
-                texto: "A educação deveria estar vinculada com o mercado de trabalho.",
-                afirmacao: "Visto que a carreira esportiva são para poucos, a educação deveria preparar para o mercado de trabalho."
-            }
-        ]
-    }
 ];
 
 let pergAtual = 0;
